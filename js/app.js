bb5525b0896c33b1b98db1026d45237c001df0ea
Shiming Liang <irisliang06@gmail.com>
2024-06-28 14:43:46 -0700

    Rock Paper Scissors Game

---
+++ js/app.js
@@ -0,0 +1,78 @@
+// 1) Define any variables used to track the state of the game:
+//    The players choice
+//    The computers choice
+//    The match result - win/lose/tie
+//    A result message - display who won that round
+
+// 2) Define the required constants:
+//    There are only 3 choices a user can make ("rock", "paper", "scissors")
+//    We'll need a reference to a DOM element to display messages
+
+// 3) Handle a player clicking a button
+
+// 4) Handle generating random selections for the computer player
+
+// 5) Compare the player choice to the computer choice, and check for a winner
+
+// 6) Render a win/lose/tie message to the player 
+//    Include both player and computer selections in the message
+//    Clearly indicate who won
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+const choices = ['rock', 'paper', 'scissors'];
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice;
+let computerChoice;
+let msg;
+
+/*------------------------ Cached Element References ------------------------*/
+const resultDisplayEl = document.getElementById('result-display');
+
+/*-------------------------------- Functions --------------------------------*/
+
+const getPlayerChoice = (e) => {
+    playerChoice = e.target.id;
+}
+
+const getComputerChoice = () => {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    computerChoice = choices[randomIndex];
+}
+
+const compare = () => {
+    if(playerChoice === computerChoice) {
+        msg = 'You tied!';
+    } else if(playerChoice === choices[0] && computerChoice === choices[2]) {
+        msg = 'Congrats! You win!';
+    } else if(playerChoice === choices[1] && computerChoice === choices[0]) {
+        msg = 'Congrats! You win!';
+    } else if(playerChoice === choices[2] && computerChoice === choices[1]) {
+        msg = 'Congrats! You win!';
+    } else {
+        msg = 'You lose! Try again?';
+    }
+}
+
+const render = () => {
+    resultDisplayEl.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}. ${msg}`;
+}
+
+const play = () => {
+    getPlayerChoice(event);
+    getComputerChoice();
+    compare();
+    render();
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+// document.getElementById('rock').addEventListener('click', play);
+// document.getElementById('paper').addEventListener('click', play);
+// document.getElementById('scissors').addEventListener('click', play);
+
+document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', play);
+})
